bf8079d5c33de4032cee0cc333d8957a22c4b895
pbs0 <122463558+pbs0@users.noreply.github.com>
2023-03-30 22:59:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ hp = 0
 print("hello world")
 
 if(energy == 0):
- print("i wanna go home")
+    print("i wanna go home")
 else(hp == 0):
- print("you should take a rest")
+    print("you should take a rest")
 
 def hanoi(n):
     return 2**1 - 1
